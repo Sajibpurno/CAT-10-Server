a823a80bb7ef3a717a7c74381323eca30c6a3033
Sajibpurno <chasajib86@gmail.com>
2026-05-19 13:23:24 +0600

    added cat data page and cards

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config()
+
+const uri = process.env.MONGODB_URI;
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('cat10')
+    const AllCatCollection = db.collection('allCards')
+    
+    app.get('/allCards', async(req, res)=>{
+      const result = await AllCatCollection.find().toArray();
+      res.json(result);
+    })
+
+    app.post('/allCards', async (req,res)=>{
+      const allCardsData =  req.body
+      console.log(allCardsData)
+      const result = await AllCatCollection.insertOne(allCardsData)
+      res.json(result)
+    })
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
